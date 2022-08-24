716ae9f1eb670106bc04d38982bcb99e155cff32
heiterpm <heiterpm@gmail.com>
2022-08-24 12:14:03 -0300

    Entity rodando no Container

--- AppWebHeiter/Program.cs
+++ AppWebHeiter/Program.cs
@@ -1,4 +1,5 @@
 using AppWebHeiter.Data;
+using AppWebHeiter.Services;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -13,6 +14,8 @@ builder.Services.AddSession();
 builder.Services.AddDistributedMemoryCache();
 
 var app = builder.Build();
+
+DatabaseManagementService.MigrationInitialisation(app);
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
---
+++ AppWebHeiter/Services/DatabaseManagementService.cs
@@ -0,0 +1,18 @@
+using AppWebHeiter.Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace AppWebHeiter.Services;
+
+public static class DatabaseManagementService
+{
+    public static void MigrationInitialisation(this IApplicationBuilder app)
+    {
+        using (var serviceScope = app.ApplicationServices.CreateScope())
+        {
+            var serviceDb = serviceScope.ServiceProvider
+                            .GetService<AppDBContext>();
+
+            serviceDb.Database.Migrate();
+        }
+    }
+}
